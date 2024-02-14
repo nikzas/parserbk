39d4b8903a49466bb813a54d6229b513390251c5
nikzas <nikudabeznego@gmail.com>
2024-02-14 06:40:22 +0300

    com

--- browser_data.py
+++ browser_data.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import Playwright, sync_playwright
 import pandas as pd
+import numpy as np
 
 class ParserAviator():
     def __init__(self):
@@ -17,7 +18,7 @@ class ParserAviator():
 
     def corr_text(self):
         massive = [float(i.replace('\xa0', '')) for i in self.run().split(sep='x') if i != '']  # Здесь  уже float
-        df = pd.DataFrame(massive).T
+        df = pd.DataFrame(massive).T # Текст повернут в горизонт
         return df
 
 if __name__ == "__main__":
@@ -26,12 +27,17 @@ if __name__ == "__main__":
     global_massive = start.corr_text()
     while True:
         next_mass = start.corr_text()
-        res_g_m = global_massive.loc[CNT, :3]   # Первые три числа в массиве
-        res_n_m = next_mass.loc[CNT, :3]
-        res_cnt = res_g_m[0]
-        result = res_g_m.compare(res_n_m)
+        first_cnt = global_massive.loc[CNT, :0]  # Первое число в глобальном массиве
+        res_n_m = next_mass.loc[CNT, :5]
 
-        find_index = result.other[result.other == res_cnt].index.tolist()
+        if first_cnt.values in res_n_m.values == True:      # Если первое число глобального массива присутсвует то Ок
+
+        index = (res_n_m == first_cnt[0]).idxmax()
+
+        result = first_cnt.compare(res_n_m)
+
+
+        #find_index = result.other[result.other == res_cnt].index.tolist()
 
         global_massive = pd.concat([global_massive, next_mass.loc[CNT, 0:find_index[[0]]]], ignore_index=True)
 
