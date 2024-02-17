b217ef9ba6e8aca9afb8b416c4c6d51f8fcd786a
nikzas <nikudabeznego@gmail.com>
2024-02-17 16:18:49 +0300

    17,02,2024

--- browser_data.py
+++ browser_data.py
@@ -1,6 +1,6 @@
-from playwright.sync_api import Playwright, sync_playwright
+from playwright.sync_api import sync_playwright
 import pandas as pd
-import numpy as np
+
 
 class ParserAviator():
     def __init__(self):
@@ -10,30 +10,37 @@ class ParserAviator():
         with sync_playwright() as p:
             browser = p.chromium.launch(headless=True)
             self.page = browser.new_page()
-            self.page.goto("https://lucky-jet.gamedev-atech.cc/?exitUrl=https%253A%252F%252F1wowei.xyz%252Fcasino&language=ru&b=demo")
+            self.page.goto("https://lucky-jet.gamedev-atech.cc/"
+                           "?exitUrl=https%253A%252F%252F1wowei.xyz%252Fcasino&language=ru&b=demo")
             self.page.locator(".sc-jIILKH > .sc-gYbzsP > .sc-hhOBVt").click()
             self.page.screenshot(path="example.png")
             self.locators = ".sc-dwnOUR"        #Для замены возможного div class
             return self.page.locator(self.locators).text_content()
 
-    def corr_text(self):
+    def corr_text_df(self):
         massive = [float(i.replace('\xa0', '')) for i in self.run().split(sep='x') if i != '']  # Здесь  уже float
-        df = pd.DataFrame({'A': massive})
+        df = pd.DataFrame(massive)
         return df
+
+    def corr_text_series(self):
+        massive = [float(i.replace('\xa0', '')) for i in self.run().split(sep='x') if i != '']
+        ser = pd.Series(massive)
+        return ser
+
+    def last_edit_text(self, global_massive, next_mass):
+        first_cnt = global_massive[0:3].stack()
+        index_find = next_mass[next_mass.isin(first_cnt)].index
+        global_massive = pd.concat([next_mass[:index_find.values[0]], global_massive])
+        print(global_massive)
+        return global_massive
+
 
 if __name__ == "__main__":
     CNT = 0
     start = ParserAviator()
-    global_massive = start.corr_text()
+    global_massive = start.corr_text_df()
     while True:
-        global_massive['B'] = start.corr_text()               #.loc[CNT, :5]
-        #first_cnt = global_massive.loc[CNT, :0]  # Первое число в глобальном массиве
-        #res_n_m = next_mass.loc[CNT, :5]
-        #if first_cnt.values in res_n_m.values:      # Если первое число глобального массива присутсвует то Ок
-            #index = res_n_m.tolist().index(first_cnt.values)
-        #result = first_cnt.compare(res_n_m)
-        #find_index = result.other[result.other == res_cnt].index.tolist()
-        #global_massive = pd.concat([global_massive, next_mass.loc[CNT, :index]], ignore_index=True)
+        next_mass = start.corr_text_series()
+        result = start.last_edit_text(global_massive, next_mass)
 
-        print(global_massive)
 
